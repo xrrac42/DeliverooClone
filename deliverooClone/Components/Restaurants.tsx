e225ee25ddb49bb536147c71649e7300bd8df350
xrrac42 <carloseduardobot@gmail.com>
2024-01-30 10:08:01 -0300

    feat restaurants details

--- deliverooClone/Components/Restaurants.tsx
+++ deliverooClone/Components/Restaurants.tsx
@@ -13,7 +13,7 @@ const Restaurants = () => {
         padding: 15,
       }}>
       {restaurants.map((restaurant, index) => (
-        <Link href={'/'} key={index} asChild>
+        <Link href={"/details"} key={index} asChild>
           <TouchableOpacity>
             <View style={styles.categoryCard}>
               <Image source={restaurant.img} style={styles.image} />
