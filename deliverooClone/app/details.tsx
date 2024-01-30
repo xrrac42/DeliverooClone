667d758236f4ea0f19d788ef5821ed338400648f
xrrac42 <carloseduardobot@gmail.com>
2024-01-30 10:54:35 -0300

    build details parallaxScrollview

--- deliverooClone/app/details.tsx
+++ deliverooClone/app/details.tsx
@@ -1,12 +1,210 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image} from 'react-native'
 import React from 'react'
+import ParallaxScrollview from "../Components/ParallaxScrollview"
+import Colors from '../constants/Colors'
+import { restaurant } from '../assets/data/restaurant'
+
+// const onScroll = (event: any) => {
+//   const y = event.nativeEvent.contentOffset.y;
+//   if (y > 350) {
+//     opacity.value = withTiming(1);
+//   } else {
+//     opacity.value = withTiming(0);
+//   }
+// };
 
 const Details = () => {
   return (
-    <View>
-      <Text>Details</Text>
-    </View>
+    <>
+         <ParallaxScrollview
+        // scrollEvent={onScroll}
+        backgroundColor={'#fff'}
+        style={{ flex: 1 }}
+        parallaxHeaderHeight={250}
+        stickyHeaderHeight={100}
+        renderBackground={() => <Image source={restaurant.img} style={{ height: 300, width: '100%' }} />}
+        contentBackgroundColor={Colors.lightGrey}
+        renderStickyHeader={() => (
+          <View key="sticky-header" style={styles.stickySection}>
+            <Text style={styles.stickySectionText}>{restaurant.name}</Text>
+          </View>
+        )}>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.restaurantName}>{restaurant.name}</Text>
+          <Text style={styles.restaurantDescription}>
+            {restaurant.delivery} · {restaurant.tags.map((tag, index) => `${tag}${index < restaurant.tags.length - 1 ? ' · ' : ''}`)}
+          </Text>
+          <Text style={styles.restaurantDescription}>{restaurant.about}</Text>
+          {/* <SectionList
+            contentContainerStyle={{ paddingBottom: 50 }}
+            keyExtractor={(item, index) => `${item.id + index}`}
+            scrollEnabled={false}
+            sections={DATA}
+            renderItem={renderItem}
+            ItemSeparatorComponent={() => <View style={{ marginHorizontal: 16, height: 1, backgroundColor: Colors.grey }} />}
+            SectionSeparatorComponent={() => <View style={{ height: 1, backgroundColor: Colors.grey }} />}
+            renderSectionHeader={({ section: { title, index } }) => <Text style={styles.sectionHeader}>{title}</Text>}
+          /> */}
+        </View>
+      </ParallaxScrollview>
+    </>
   )
 }
 
-export default Details
+const styles = StyleSheet.create({
+  detailsContainer: {
+    backgroundColor: Colors.lightGrey,
+  },
+  stickySection: {
+    backgroundColor: '#fff',
+    marginLeft: 70,
+    height: 100,
+    justifyContent: 'flex-end',
+  },
+  roundButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+  },
+  stickySectionText: {
+    fontSize: 20,
+    margin: 10,
+  },
+  restaurantName: {
+    fontSize: 30,
+    margin: 16,
+  },
+  restaurantDescription: {
+    fontSize: 16,
+    margin: 16,
+    lineHeight: 22,
+    color: Colors.medium,
+  },
+  sectionHeader: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 40,
+    margin: 16,
+  },
+  item: {
+    backgroundColor: '#fff',
+    padding: 16,
+    flexDirection: 'row',
+  },
+  dishImage: {
+    height: 80,
+    width: 80,
+    borderRadius: 4,
+  },
+  dish: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  dishText: {
+    fontSize: 14,
+    color: Colors.mediumDark,
+    paddingVertical: 4,
+  },
+  stickySegments: {
+    position: 'absolute',
+    height: 50,
+    left: 0,
+    right: 0,
+    top: 100,
+    backgroundColor: '#fff',
+    overflow: 'hidden',
+    paddingBottom: 4,
+  },
+  segmentsShadow: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 5,
+    width: '100%',
+    height: '100%',
+  },
+  segmentButton: {
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+    borderRadius: 50,
+  },
+  segmentText: {
+    color: Colors.primary,
+    fontSize: 16,
+  },
+  segmentButtonActive: {
+    backgroundColor: Colors.primary,
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+    borderRadius: 50,
+  },
+  segmentTextActive: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  segmentScrollview: {
+    paddingHorizontal: 16,
+    alignItems: 'center',
+    gap: 20,
+    paddingBottom: 4,
+  },
+  footer: {
+    position: 'absolute',
+    backgroundColor: '#fff',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    padding: 10,
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: -10 },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    paddingTop: 20,
+  },
+  fullButton: {
+    backgroundColor: Colors.primary,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    alignItems: 'center',
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-between',
+    height: 50,
+  },
+  footerText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  basket: {
+    color: '#fff',
+    backgroundColor: '#19AA86',
+    fontWeight: 'bold',
+    padding: 8,
+    borderRadius: 2,
+  },
+  basketTotal: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+export default Details;
